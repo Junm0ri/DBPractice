4969acf6608bc9586716617b97e7c7c5bc37854f
Jun Moriuchi <79455149+Junm0ri@users.noreply.github.com>
2021-12-20 14:18:46 +0900

    INPUTボタンを実装。 押すと入力ダイアログが生成され、入力文字列とそのときの感性値をDBに追加

--- app/src/main/java/com/example/dbpractice/MainActivity.kt
+++ app/src/main/java/com/example/dbpractice/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.dbpractice
 
 import android.app.Activity
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dbpractice.databinding.ActivityMainBinding
 import io.realm.Realm
@@ -46,6 +50,28 @@ class MainActivity : AppCompatActivity() {
             realm.executeTransaction { db:Realm->
                 db.where<Schedule>()?.findFirst()?.deleteFromRealm()
             }
+        }
+
+        binding.InputButton.setOnClickListener {
+            val myedit = EditText(this)
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle("文字を入力してください")
+            dialog.setView(myedit)
+            dialog.setPositiveButton("OK", DialogInterface.OnClickListener { _, _ ->
+                val userText = myedit.getText().toString()
+                Toast.makeText(this, "$userText と入力しました", Toast.LENGTH_SHORT).show()
+                //DBに追加
+                realm.executeTransaction { db:Realm->
+                    val maxId=db.where<Schedule>().max("id")
+                    val nextId=(maxId?.toLong() ?:0L)+1L
+                    val schedule=db.createObject<Schedule>(nextId)
+                    schedule.name=userText
+                    schedule.Kansei=binding.NumberEdit.text.toString()
+                }
+            })
+            dialog.setNegativeButton("キャンセル", null)
+            dialog.show()
+
         }
 
         //アダプターのアイテムを押した時の処理
